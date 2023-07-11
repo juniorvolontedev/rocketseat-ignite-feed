13080800988a5a4faf6e5c5e5ed268e1a4de5882
Junior Volonte <juniorvolontedev@gmail.com>
2023-07-11 07:06:15 -0300

    first commit

---
+++ src/components/Comment.tsx
@@ -0,0 +1,59 @@
+import { ThumbsUp, Trash } from "phosphor-react";
+import { Avatar } from "./Avatar";
+import styles from "./Comment.module.css";
+import { useState } from "react";
+
+interface CommentProps {
+  content: string;
+  onDeleteComment: (comment: string) => void;
+}
+
+export function Comment({ content, onDeleteComment }: CommentProps) {
+  const [likeCount, setLikeCount] = useState(0);
+
+  function handleDeleteComment() {
+    onDeleteComment(content);
+  }
+
+  function handleLikeComment() {
+    setLikeCount((state) => {
+      return state + 1;
+    });
+  }
+
+  return (
+    <div className={styles.comment}>
+      <Avatar
+        hasBorder={false}
+        src="https://github.com/juniorvolontedev.png"
+        alt=""
+      />
+
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Diego Fernandes</strong>
+              <time title="11 de Maio às 08h11" dateTime="2022-05-11 08:11:33">
+                Cerca de 1h atrás
+              </time>
+            </div>
+
+            <button title="Deletar comentário" onClick={handleDeleteComment}>
+              <Trash size={24} />
+            </button>
+          </header>
+
+          <p>{content}</p>
+        </div>
+
+        <footer>
+          <button onClick={handleLikeComment}>
+            <ThumbsUp />
+            Aplaudir <span>{likeCount}</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  );
+}
